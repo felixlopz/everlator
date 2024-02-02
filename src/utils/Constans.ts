1d76c03fe2acb48aa7cbd7a25b0c74e0b38aab32
Felix Lopez <felixlopzd@gmail.com>
2024-02-02 00:02:00 -0400

    updating prices

--- src/utils/Constans.ts
+++ src/utils/Constans.ts
@@ -1,7 +1,7 @@
 export const PACKAGING_SERVICE = 5;
-export const PRICE_PER_CUBIC_FOOT = 30;
+export const PRICE_PER_CUBIC_FOOT = 31;
 export const PRICE_PER_POUND = 5;
-export const BASE_AIR_SHIPPING_PRICE = 21;
-export const BASE_OCEAN_SHIPPING_PRICE = 30;
+export const BASE_AIR_SHIPPING_PRICE = 30;
+export const BASE_OCEAN_SHIPPING_PRICE = 40;
 export const CUBIC_FOOT_DIVISOR = 1728;
 export const LOCAL_STORAGE_DATA_KEY = "evercalculator-items";
